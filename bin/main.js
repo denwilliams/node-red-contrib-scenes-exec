77970cabcba43fdbc23a97247df15e1e601ac580
Den Williams <work@denwilliams.net>
2016-05-22 15:04:05 +1000

    use compiled typescript

--- bin/main.js
+++ bin/main.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
 
 // script to execute node red
-
-var RED = require('node-red');
-var main = require('node-red-contrib-scenes');
-main.start(RED, {port:1880});
+require('../lib');
